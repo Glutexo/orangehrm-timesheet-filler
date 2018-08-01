d89f92f1d39e467dda8e2f673c0630b696502573
Glutexo <glutexo@icloud.com>
2018-08-01 07:31:09 +0200

    Use a button to fill the timesheet
    Instead of filling the timesheet automatically, put a Fill button above the form. This buttons triggers the filling. UX!

--- OrangeHRM timesheet filler.safariextension/main.js
+++ OrangeHRM timesheet filler.safariextension/main.js
@@ -7,36 +7,59 @@
  */
 
 (function() {
-	var rows = document.querySelectorAll('#timesheetTable .activeRow');
+    function fillTimeSheet() {
+		var rows = document.querySelectorAll('#timesheetTable .activeRow');
 
-	function eachRow(row, _index, _list) {
-		// @TODO Make less cryptic. :)
-		var regularDay = row.classList.length == 1 && row.classList[0] == 'activeRow';
-	
-		function fill() {
-			var inTimeInput = row.querySelector('[name$=\\[inTime\\]]'),
-				outTimeInput = row.querySelector('[name$=\\[outTime\\]]'),
-				breakDurationInput = row.querySelector('[name$=\\[breakDuration\\]]'),
-				event = new MouseEvent('blur');
-				empty = inTimeInput.value === '' && outTimeInput.value === '' && breakDurationInput.value === '';
-	    
-			// Only empty rows, do not overwrite something special.
-			if (empty) {
-				// 8 hrs of work, ½ hr lunch break
-				inTimeInput.value = '08:00';
-				outTimeInput.value = '16:30';
-				breakDurationInput.value = '00:30';
+		function eachRow(row, _index, _list) {
+			// @TODO Make less cryptic. :)
+			var regularDay = row.classList.length == 1 && row.classList[0] == 'activeRow';
+		
+			function fillRow() {
+				var inTimeInput = row.querySelector('[name$=\\[inTime\\]]'),
+					outTimeInput = row.querySelector('[name$=\\[outTime\\]]'),
+					breakDurationInput = row.querySelector('[name$=\\[breakDuration\\]]'),
+					event = new MouseEvent('blur');
+					empty = inTimeInput.value === '' && outTimeInput.value === '' && breakDurationInput.value === '';
+			
+				// Only empty rows, do not overwrite something special.
+				if (empty) {
+					// 8 hrs of work, ½ hr lunch break
+					inTimeInput.value = '08:00';
+					outTimeInput.value = '16:30';
+					breakDurationInput.value = '00:30';
 
-				// Must trigger a blur event, so the app counts the working hours for the day.
-				breakDurationInput.dispatchEvent(event);
+					// Must trigger a blur event, so the app counts the working hours for the day.
+					breakDurationInput.dispatchEvent(event);
+				}
+			}
+
+			// Only regular days, everything else is a special case.
+			if (regularDay) {
+				fillRow();
 			}
 		}
+		
+		rows.forEach(eachRow);
+    }
 
-		// Only regular days, everything else is a special case.
-		if (regularDay) {
-			fill();
+    function init() {
+		var saveButton = document.getElementById('topBtnSave'),
+			formGroup = saveButton.parentNode,
+			fillButton = document.createElement('input');
+
+		function buttonClick(event) {
+			event.preventDefault();
+			fillTimeSheet();
 		}
-	}
-    
-	rows.forEach(eachRow);
+
+		fillButton.type = 'button';
+		fillButton.value = 'Fill';
+		fillButton.classList.add('btn', 'btn-sm', 'rh-grn');
+
+		fillButton.addEventListener('click', buttonClick);
+		
+		formGroup.appendChild(fillButton);
+    }
+
+    init();
 })();
